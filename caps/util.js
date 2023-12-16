502de7800c3fd5c44698a372503bb58a5acc68dd
brosmar18 <bryangonzalez1896@icloud.com>
2023-12-16 13:35:16 -0600

    util updated to include vendorQueue

--- caps/util.js
+++ caps/util.js
@@ -4,13 +4,13 @@ const clientSqs = require('@aws-sdk/client-sqs');
 
 const { SQSClient } = clientSqs;
 
-
 const REGION = 'us-east-1';
 const sqsClient = new SQSClient({ region: REGION });
 
 const QUEUES = {
   pickup: 'https://sqs.us-east-1.amazonaws.com/470644768484/capsPickup.fifo',
+  vendorQueue: 'https://sqs.us-east-1.amazonaws.com/470644768484/vendor.fifo', 
+
 };
 
 module.exports = { sqsClient, QUEUES };
-
