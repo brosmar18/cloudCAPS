0c3b98e59daf855a375e1af25324a0b87a34593b
brosmar18 <bryangonzalez1896@icloud.com>
2023-12-16 13:18:27 -0600

    vendor updated to publish messages to SNS topic

--- caps/vendor/handler.js
+++ caps/vendor/handler.js
@@ -1,38 +1,43 @@
-const { SendMessageCommand } = require('@aws-sdk/client-sqs');
-const { sqsClient, QUEUES } = require('../util');
-const Chance = require('chance');
-const chance = new Chance;
+'use strict';
 
+const AWS = require('aws-sdk');
+const Chance = require('chance');
+const { sqsClient, QUEUES } = require('../util');
+
+AWS.config.update({ region: 'us-east-1' });
+const sns = new AWS.SNS();
+const chance = new Chance();
+const topicArn = 'arn:aws:sns:us-east-1:470644768484:capsMessages'; 
 const store = '1-206-flowers';
 
-async function sendPickup(){
+async function sendPickup() {
   const payload = {
     store,
     orderId: chance.guid(),
     customer: chance.name(),
     address: chance.address(),
+    vendorUrl: QUEUES.vendorQueue, 
   };
+
   console.log('Vendor asking for pickup', payload);
 
-  try{
-    const message = await sqsClient.send(
-      new SendMessageCommand({
-        MessageBody: JSON.stringify(payload),
-        MessageGroupId: store,
-        QueueUrl: QUEUES.pickup,
-      })
-    );
-    console.log('vendor sent pickup request', message.MessageId);
+  try {
+    const message = await sns.publish({
+      Message: JSON.stringify(payload),
+      TopicArn: topicArn,
+    }).promise();
+
+    console.log('Vendor sent pickup request', message.MessageId);
     return message;
-  } catch (e){
-    console.error('failed to send the pickup message', e)
+  } catch (e) {
+    console.error('Failed to send the pickup message', e);
   }
 }
 
-function startVendor(){
+function startVendor() {
   setInterval(() => {
     sendPickup();
-  }, 5000);
+  }, 5000); 
 }
 
 module.exports = startVendor;
